55143cf77a35ebfed6f28fd422d1acaa6cbd8d45
bnjj01 <bepoamhu.2016@gmail.com>
2026-05-03 12:00:28 -0300

    cambios varios

--- TPN4/ejercicio09.js
+++ TPN4/ejercicio09.js
@@ -24,15 +24,25 @@ const tareasPendientes = [
         "completada" : true
     }
 ]
-function haySinCmpoletar(){
-    
+function cuantasCompletadas() {
+       let total = tareasPendientes.reduce((contador, tarea) => {
+           return tarea.completada ? contador + 1 : contador;
+       }, 0);
+       console.log(total);
+   }
+// cuantasCompletadas();
+
+function haySinCompletar(){
+    const algunaSinCompletar= (tarea) => tarea.completada === false;
+    console.log(tareasPendientes.some(algunaSinCompletar));
 }
+// haySinCompletar()
 
 function tareasCompletadas (){
     const estaCompletada = (tarea) => tarea.completada;
     console.log(tareasPendientes.every(estaCompletada));
 }
-
+// tareasCompletadas()
 function tareasIncompletas(){
     let tareasIncompletas = tareasPendientes.filter((tarea)=> tarea.completada === false)
     console.log(tareasIncompletas);
---
+++ TPN4/ejercicio_10.js
@@ -0,0 +1,2 @@
+let elemento = document.getElementById("formulario-color");
+document.write(elemento);
