fb9c45d4bf3aa718fbe92506da0509d38651033c
benja <bepoamhu.2016@gmail.com>
2026-05-03 14:09:18 -0300

    cambios varios

--- TPN4/ejercicio_10.js
+++ TPN4/ejercicio_10.js
@@ -1,2 +1,15 @@
-let elemento = document.getElementById("formulario-color");
-document.write(elemento);
+
+const selector = document.getElementById("select-color");
+const limpiar = document.getElementById("btn-limpiar");
+const botonCambiar = document.getElementById("btn-cambiar");
+
+botonCambiar.addEventListener("click", function(evento){
+    evento.preventDefault();
+    const colorElegido = selector.value;
+    document.body.style.backgroundColor = colorElegido;
+});
+
+limpiar.addEventListener("click", function(evento){
+    evento.preventDefault();
+    document.body.style.backgroundColor = "white";
+});
