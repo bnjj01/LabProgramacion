2a7219a91979feba34c8169ff4d1d3eebf0aca90
benja <bepoamhu.2016@gmail.com>
2026-05-22 00:34:28 -0300

    avances con javascript

--- CursoJavaScript/buclesEIteradores.js
+++ CursoJavaScript/buclesEIteradores.js
@@ -1,4 +1,46 @@
 /*
+// sentencia label
+const array1 = ["oriana","claudia","puyol"]
+const array2 = ["pedro","marcelo",array1,"Claudio"]
+
+forLabel:
+for (let array in array2){
+    // recorriendo arrays
+    if(array == 2){
+        for(let array of array1){
+            document.write(array+"<br>");
+            break forLabel;
+        }
+    }else{
+        document.write(array2[array]+"<br>")
+    }
+}
+*/
+/*
+//for in, 
+const animales =["Gato","Perro", "Elefante"]
+//la variable animal toma la posicion de cada elemnto.
+for(let animal in animales){
+    document.write(animal+"<br>");
+}
+// for of
+//la variable animal toma el valor de cada elemento.
+for(let animal of animales){
+    document.write(animal+"<br>");
+}
+
+//for
+for(let i = 0; i < 20; i++){
+    if (i == 15){
+        continue;
+        //break;
+    }
+    document.write(i+"<br>");
+}
+ 
+*/
+
+/*
 //break
 let numero = 0;
 while(numero < 1000){
@@ -29,4 +71,3 @@ while(contador < numero){
     document.write("contador es igual a: "+ contador + "<br>");
 }
 document.write("numero es igual a: "+numero+ "<br>");*/
-
---
+++ CursoJavaScript/funciones.js
@@ -0,0 +1,61 @@
+//funciones flecha
+const saludar = nombre=>{
+    let frase = `Hola ${nombre} ¿Como estas?`
+    document.write(frase);
+}
+
+saludar("Benja Anashex")
+// const saludar = function(nombre){
+//     let frase = `Hola ${nombre} ¿Como estas?`;
+//     document.write(frase);
+// }
+// saludar("Benja");
+
+/*
+//Parametros
+function saludar(nombre){
+    let frase = `Hola ${nombre} como estas??`
+    return frase;
+}
+let nombre = prompt("ingresa tu nombre: ");
+
+document.write(saludar(nombre));
+
+function suma(a, b){
+    let res = a + b;
+    return res;
+}
+
+document.write(suma(3));
+document.write("<br>");*/
+
+/*
+//el return devuelve un dato y termina la funcion.
+function saludar(){
+    alert("hola")
+    return "la funcion se ejecuto correctamente"
+}
+
+let saludo = saludar();
+document.write(saludo)
+
+//Formas de declara funciones
+saludar= function(){
+    respuesta = prompt("Hola Benja, como estas?");
+    if(respuesta == "bien"){
+        alert("me alegro pibe")
+    }else {
+        alert("que lastima")
+    }
+}
+*/
+/*
+function saludar(){
+    respuesta = prompt("Hola Benja, como estas?");
+    if(respuesta == "bien"){
+        alert("me alegro pibe")
+    }else {
+        alert("que lastima")
+    }
+}
+*/
