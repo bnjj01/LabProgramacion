310b72a32416c3aa451f227e3abdbd9a9c425142
bnjj01 <bepoamhu.2016@gmail.com>
2026-05-29 04:16:10 -0300

    agg carpetas del pi

--- lab_prog_2026_Pozzo_Benjamin/public/app/js/user/index.js
+++ lab_prog_2026_Pozzo_Benjamin/public/app/js/user/index.js
@@ -1,1 +1,16 @@
 import userController from "./controller.js";
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    userController.list();
+
+    const botonExportar = document.getElementById('btn-exportar');
+
+    if (botonExportar){
+        botonExportar.addEventListener('click', function(evento){
+                evento.preventDefault();
+
+                userController.exportToPDF();
+        })
+    }
+})
